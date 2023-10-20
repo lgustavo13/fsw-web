98fb03416f594748342a31eded4d5eae9b583279
lsilva19 <lsilva19@lenovo.com>
2023-10-19 23:43:46 -0300

    style: change cart width

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -123,7 +123,7 @@ const Header = () => {
           </Button>
         </SheetTrigger>
 
-        <SheetContent>
+        <SheetContent className="w-[350px]">
           <Cart />
         </SheetContent>
       </Sheet>
